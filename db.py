a4dfce096f0a57f7014e1b05c10a725e5cdf34ca
Rafael Fakhrutdinov <rfakhrutdinov@fix.ru>
2020-06-27 20:48:44 +0300

    DR_3 refactoring

--- db.py
+++ db.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 
-from configs import conf
-from eating import Eating
 from app import conn
+from configs import conf
 
 
 class DailyRation:
@@ -17,13 +16,6 @@ class DailyRation:
         day = marathon_day_num % 7 + 1
         return [week, day]
 
-    @staticmethod
-    def format_field(field_name, value):
-        if field_name == "eating":
-            return Eating.verbose(value)
-        else:
-            return value
-
     @classmethod
     async def get_daily_ration(cls):
         week, day = cls.get_current_week_and_day()
@@ -36,51 +28,47 @@ class DailyRation:
         rows = await cursor.fetchall()
         return [
             {
-                field: cls.format_field(field, row[i])
-                for i, field in enumerate(cls.select_fields)
+                field: row[i] for i, field in enumerate(cls.select_fields)
             } for row in rows
         ]
 
     @classmethod
     async def get_today_eating(cls, message):
         week, day = cls.get_current_week_and_day()
-        eating = Eating.get_key_by_value(message)
         today_eating_sql = f"""
             SELECT {', '.join(cls.select_fields)}
             FROM recipes
-            WHERE week = {week} AND day = {day} AND eating = {eating};
+            WHERE week = {week} AND day = {day} AND eating = '{message}';
         """
         cursor = await conn.execute(today_eating_sql)
         row = await cursor.fetchone()
         return {
-            field: cls.format_field(field, row[i])
-            for i, field in enumerate(cls.select_fields)
+            field: row[i] for i, field in enumerate(cls.select_fields)
         }
 
     @classmethod
     async def get_random_eating(cls, message):
-        eating = Eating.get_key_by_value(message[6:])
         today_eating_sql = f"""
             SELECT {', '.join(cls.select_fields)}
             FROM recipes
-            WHERE eating = {eating}
+            WHERE eating = '{message[6:]}'
             ORDER BY RANDOM() LIMIT 1;
         """
         cursor = await conn.execute(today_eating_sql)
         row = await cursor.fetchone()
         return {
-            field: cls.format_field(field, row[i])
-            for i, field in enumerate(cls.select_fields)
+            field: row[i] for i, field in enumerate(cls.select_fields)
         }
 
     @classmethod
     async def get_recipe_by_str(cls, message):
         message_text = message.text
-        err = None
-        if len(message_text) < 4:
-            err = "Длина поисковой строки должна быть > 3 символов"
+        err_msg = "Длина поисковой строки должна быть > 3 символов"
+        err = err_msg if len(message_text) < 4 else None
         lower_message = message_text.lower()
         capitalized_message = message_text.capitalize()
+        # sqlite does not have case-sensitive search
+        # upper/lower functions do not work with Unicode
         search_by_str = f"""
             SELECT {', '.join(cls.select_fields)}
             FROM recipes
@@ -94,8 +82,7 @@ class DailyRation:
         cursor = await conn.execute(search_by_str)
         rows = await cursor.fetchall()
         return [
-            {
-                field: cls.format_field(field, row[i])
-                for i, field in enumerate(cls.select_fields)
-            } for row in rows
+           {
+               field: row[i] for i, field in enumerate(cls.select_fields)
+           } for row in rows
         ], err
--- scrapper.py
+++ scrapper.py
@@ -11,9 +11,11 @@ from configs import conf
 
 
 class Scrapper:
-    weeks = range(1, 9)  # 8 weeks
+    weeks = range(1, conf["marathon_duration"] + 1)
     days_in_week = range(1, 8)  # 7 days in week
     all_days_with_recipes = itertools.product(weeks, days_in_week)
+
+    eating = ["Завтрак", "Перекус 1", "Обед", "Перекус2", "Ужин"]
 
     login_url = f"{conf['parser']['main_page']}/login"
     ration_url = f"{conf['parser']['main_page']}/marafon/food-custom"
@@ -53,19 +55,19 @@ class Scrapper:
 
     async def parse_daily_ration(self, content, week, day):
         parsed_ration = content.find_all("div", class_="food_item")
-        for ind, eating in enumerate(parsed_ration):
-            title = self.get_recipe_title(eating)
-            ingredients = self.get_recipe_ingredients(eating)
-            cooking = self.get_recipe_cooking(eating)
-            pic_name = await self.save_recipe_picture(eating)
-            row = (week, day, ind + 1, title, ingredients, cooking, pic_name)
+        for ind, meal in enumerate(parsed_ration):
+            title = self.get_recipe_title(meal)
+            ingredients = self.get_recipe_ingredients(meal)
+            cook = self.get_recipe_cooking(meal)
+            pic = await self.save_recipe_picture(meal)
+            row = (week, day, self.eating[ind], title, ingredients, cook, pic)
             self.recipes.append(row)
 
     async def save_all_recipes(self):
         async with aiosqlite.connect(conf["db_name"]) as conn:
             await conn.execute("""
                 CREATE TABLE IF NOT EXISTS recipes
-                (week integer, day integer, eating integer, title text,
+                (week integer, day integer, eating string, title text,
                 ingredients text, cooking text, pic_name text)
             """)
             await conn.executemany("""
@@ -75,21 +77,21 @@ class Scrapper:
             await conn.commit()
 
     @staticmethod
-    def get_recipe_title(eating):
-        return eating.find("div", class_="food_item_name").text
+    def get_recipe_title(meal):
+        return meal.find("div", class_="food_item_name").text
 
     @staticmethod
-    def get_recipe_ingredients(eating):
-        ingredients_divs = eating.find_all("td", class_="ingredients")
+    def get_recipe_ingredients(meal):
+        ingredients_divs = meal.find_all("td", class_="ingredients")
         return "\n".join([ingr.text for ingr in ingredients_divs])
 
     @staticmethod
-    def get_recipe_cooking(eating):
-        cooking_div = eating.find("div", class_="cooking")
+    def get_recipe_cooking(meal):
+        cooking_div = meal.find("div", class_="cooking")
         return cooking_div.text if cooking_div else ""
 
-    async def save_recipe_picture(self, eating):
-        pic_styles_div = eating.find("div", class_="food_item_img")
+    async def save_recipe_picture(self, meal):
+        pic_styles_div = meal.find("div", class_="food_item_img")
         pic_div_styles = pic_styles_div["style"] if pic_styles_div else ""
         pic_name = ""
         if not pic_div_styles:
